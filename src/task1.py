84a4be11b1f0bcada8dc2f3c3f3150d4cb589aeb
Usman Ahmad <uah5512@gmail.com>
2022-09-13 15:09:18 +0300

    Generalizing Task1 decorator for arbitrary no of args

--- src/task1.py
+++ src/task1.py
@@ -7,11 +7,11 @@ def decorator_1(base):
     """prints function execution time and the number of times it was called"""
     
     no_of_calls = 0
-    def f(**args):
+    def f(*args, **kwargs):
         nonlocal no_of_calls
         st = time.time()
         with contextlib.redirect_stdout(io.StringIO()) as _:
-            base(**args)
+            base(*args, **kwargs)
         exec_time = time.time() - st
         no_of_calls += 1
         print(f"func {base.__name__} {no_of_calls} executed in {round(exec_time, 4)} sec")    
