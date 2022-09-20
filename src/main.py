824436d46f61c994d33d92a36a35ce3eb338c15e
Usman Ahmad <uah5512@gmail.com>
2022-09-20 10:43:14 +0300

    main implemented

--- src/main.py
+++ src/main.py
@@ -1,42 +1,53 @@
 import random
 from task1 import decorator_1
 from task2 import decorator_2
-from task3 import SpecsMetrics, plot_table
+from task3 import decorator_3, plot_rank_table
 
 
-@SpecsMetrics
+@decorator_3
+# @decorator_2
 def pascal_triangle(n):
     """Calculates and returns pascal triangle"""
-    results = [] # a container to collect the rows
+    res = [] # a container to collect the rows
     for _ in range(n): 
         row = [1] # a starter 1 in the row
-        if results: # then we're in the second row or beyond
-            last_row = results[-1] # reference the previous row
-            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+        if res: # then we're in the second row or beyond
+            last_row = res[-1] # reference the previous row
+            row.extend([sum(last_rows) for last_rows in zip(last_row, last_row[1:])])
             # finally append the final 1 to the outside
             row.append(1)
-        results.append(row) # add the row to the results.
-
-    return results
-
-# @SpecsMetrics
-# def quadratic_solver(a, b, c):
-#     """Solve Quadratic Equation"""
-#     partial_num = (b ** 2 - 4 * a * c) ** (1/2)
-#     denominator = 2 * a
-#     first_root = ((-1 * b) + partial_num) / denominator
-#     second_root = ((-1 * b) - partial_num) / denominator
-#     return first_root, second_root
+        res.append(row) # add the row to the results.
+    return res
 
 
-@SpecsMetrics
+@decorator_3
+# @decorator_2
+def quadratic_solver(a, b, c):
+    """Solve quadratic equation roots"""
+    partial_num = (b ** 2 - 4 * a * c) ** (1/2)
+    denominator = 2 * a
+    first_root = ((-1 * b) + partial_num) / denominator
+    second_root = ((-1 * b) - partial_num) / denominator
+    return first_root, second_root
+
+
+@decorator_3
+def power_solver(num, power):
+    """Solves power for a num"""
+    pow = lambda n: lambda p: n ** p
+    return pow(num)(power)
+
+
+# @decorator_3
+@decorator_1
 def quadratic_solver(a, b, c):
     """Solves Quadratic Equation"""
     quadratic = lambda a, b, c: ((((-1 * b) + ((b ** 2 - 4 * a * c) ** (1/2))) / 2 * a),\
                                     (((-1 * b) - ((b ** 2 - 4 * a * c) ** (1/2))) / 2 * a))
     return quadratic(a, b, c)
 
-@SpecsMetrics
+@decorator_3
+# @decorator_2
 def func():
     """Func2 does something great!"""
     print("I am ready to Start")
@@ -45,39 +56,38 @@ def func():
     for i in range(n):
         result += (i**2)
         
-# @PrintSpecs
-# def funx(n=2, m=5):
-#     """funx does something excellent!"""
-#     print("I am ready to do serious stuff")
-#     max_val = float('-inf')
-#     n =  random.randint(10,751)
-#     res = [pow(i,2) for i in range(n)]
-#     for i in res:
-#         if i > max_val: 
-#             max_val = i
-    
-if __name__ == "__main__": 
-    # func()
-    # funx()
-    # func()
-    # funx()
-    func()
-    pascal_triangle(3)
-    # pascal_triangle(190)
-    # quadratic_solver(4434525435343243, 4434525435343243, 4434525435343243)
-    quadratic_solver(32, 43, 23)    
-    plot_table()
+@decorator_3
+def funx(n=2, m=5):
+    """funx does something excellent!"""
+    print("I am ready to do serious stuff")
+    max_val = float('-inf')
+    n =  random.randint(10,751)
+    res = [pow(i,2) for i in range(n)]
+    for i in res:
+        if i > max_val: 
+            max_val = i
 
 
 # @decorator_1
-# @decorator_2
-# def funh(bar1, bar2=""):
-#     """This function does something useful 
-#     :param bar1: description
-#     :param bar2: description
-#     """ 
-#     print("some\n\t multiline\n\t output")
+@decorator_2
+def funh(bar1, bar2=""):
+    """This function does something useful 
+    :param bar1: description
+    :param bar2: description
+    """ 
+    print("some\n\t multiline\n\t output")
 
 
-# if __name__ == "__main__": 
-#     funh(None, bar2="")
+if __name__ == "__main__": 
+    func()
+    # funx()
+    func()
+    # funx()
+    func()
+    pascal_triangle(3)
+    pascal_triangle(190)
+    quadratic_solver(4434525435343243, 4434525435343243, 4434525435343243)
+    quadratic_solver(32, 43, 23)    
+    funh('32')
+    power_solver(3, 4)
+    plot_rank_table()
--- src/task3.py
+++ src/task3.py
@@ -4,36 +4,37 @@ import contextlib
 from task4 import ErrorLogsDecorator
 
 
-metrics = {}   # store function execution metrics
+rank_metrics = {}   # store function execution metrics
 OUT_FILE = 'logs.txt'  # path where decorator logs will be dumped
 
 
-def plot_table():
+def plot_rank_table():
     """Plot metrics table sorted by execution time"""
-    sorted_metrics = sorted(metrics.items(), key=lambda item: item[1])
+    sorted_metrics = sorted(rank_metrics.items(), key=lambda item: item[1])
     print('{: <17} | {: ^13} | {: ^15}'.format("PROGRAM", "RANK", "TIME"))
     for rank, fn_exec_time in enumerate(sorted_metrics):
         fn, exec_time = fn_exec_time
         print('{: <17}  {: ^15}  {: ^17}'.format(fn, str(rank + 1), exec_time))
 
 
-def Decorator(ff):
+def DecLogsHandler(fn):
+    """Dumps logs of decorated function(s) in a txt file"""
+
     @ErrorLogsDecorator
     def wrapper(*args):
         fdesc = open(OUT_FILE, "a")
-        with contextlib.redirect_stdout(fdesc) as k:
-
-            return ff(*args)
+        with contextlib.redirect_stdout(fdesc) as _:
+            return fn(*args)
     return wrapper
 
 
-class SpecsMetrics:
+class decorator_3:
     """Prints function execution time and the number of times it was called"""
     def __init__(self, function):
         self.function = function
         self.counter = 0
 
-    @Decorator
+    @DecLogsHandler
     def myfun(self, *args):
         st = time.time()
         global OUT_FILE
@@ -44,10 +45,10 @@ class SpecsMetrics:
         exec_time = time.time() - st
         fname = self.function.__name__
 
-        metrics[fname] = exec_time
+        rank_metrics[fname] = exec_time
         print(f"func {fname} {self.counter} executed in {exec_time} sec")    
 
-    @Decorator
+    @DecLogsHandler
     def myfun2(self, *args, **kwargs):
         print("Name:\t", self.function.__name__)
         print("Type:\t", type(self.function))
@@ -56,7 +57,7 @@ class SpecsMetrics:
         print("Source: ", end=' ')
         print('\n\t '.join(inspect.getsource(self.function).split('\n')))
         print("Output:\t ", end='')
-        # Uncomment this line to test exception handling functionality
+        # Uncomment this line to test exception handling functionality for decorator_4
         # raise ValueError("Some Value Error")
         self.function(*args, **kwargs)
 
